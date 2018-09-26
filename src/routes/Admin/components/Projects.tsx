3afa4a33790682058a0f090ed867c258b372a11c
Alexandros Dorodoulis <aldor94@gmail.com>
2018-09-26 20:36:48 +0200

    feat: Update project labels on admin panel

--- src/routes/Admin/components/Projects.tsx
+++ src/routes/Admin/components/Projects.tsx
@@ -26,16 +26,16 @@ export const ProjectList = (props: any) => (
   <List {...props}>
     <Datagrid>
       <TextField source="id" />
-      <TextField source="display_name" />
+      <TextField source="display_name" label="Project Name" />
       <TextField source="first_color" />
       <TextField source="second_color" />
       <TextField source="display_name" />
       <NumberField source="issues_count" />
       <TextField source="setup_duration" />
-      <TextField source="logo" />
-      <TextField source="description" />
+      <TextField source="logo" label="Logo url (*.png, *.jpg etc)" />
+      <TextField source="description" label=" Short project description" />
       <TagsField label="Tags" source="tags" />
-
+      <TextField source="link" label="Link to GitHub repo" />
       <EditButton />
     </Datagrid>
   </List>
@@ -49,14 +49,14 @@ export const ProjectEdit = (props: any) => (
   <Edit title={<ProjectTitle />} {...props}>
     <SimpleForm>
       <DisabledInput source="id" />
-      <TextInput source="display_name" />
+      <TextInput source="display_name" label="Project Name" />
       <TextInput source="first_color" />
       <TextInput source="second_color" />
       <NumberInput source="issues_count" />
       <TextInput source="setup_duration" />
-      <TextInput source="link" />
-      <TextInput source="logo" />
-      <LongTextInput source="description" />
+      <TextInput source="link" label="Link to GitHub repo" />
+      <TextInput source="logo" label="Logo url (*.png, *.jpg etc)" />
+      <LongTextInput source="description" label=" Short project description" />
     </SimpleForm>
   </Edit>
 );
@@ -64,14 +64,14 @@ export const ProjectEdit = (props: any) => (
 export const ProjectCreate = (props: any) => (
   <Create {...props}>
     <SimpleForm>
-      <TextInput source="display_name" />
+      <TextInput source="display_name" label="Project Name" />
       <TextInput source="first_color" />
       <TextInput source="second_color" />
       <NumberInput source="issues_count" />
       <TextInput source="setup_duration" />
-      <TextInput source="link" />
-      <TextInput source="logo" />
-      <LongTextInput source="description" />
+      <TextInput source="link" label="Link to GitHub repo" />
+      <TextInput source="logo" label="Logo url (*.png, *.jpg etc)" />
+      <LongTextInput source="description" label=" Short project description" />
     </SimpleForm>
   </Create>
 );
