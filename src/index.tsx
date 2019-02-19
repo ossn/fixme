9fecef0be70c20344a75060f7f67d42760cbfba5
siddhant1 <sidhant.manchanda@gmail.com>
2019-02-19 17:53:33 +0530

    Removed Redundant Serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import "./styles/core.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
