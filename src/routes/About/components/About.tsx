4e6a173e44b364ae60340ea2ec45c7b9a5b5c6e7
ashu8912 <ashughildiyal5@gmail.com>
2019-03-01 07:34:24 +0530

    Vertically aligned text in about page

--- src/routes/About/components/About.tsx
+++ src/routes/About/components/About.tsx
@@ -30,7 +30,7 @@ export default class About extends React.PureComponent {
                     <section className=" container">
                         <div className="container">
                             <div className="row mt-5 mb-5">
-                                <div className="col-md-6">
+                                <div className="col-md-6  my-auto">
                                     <h3 className="about-sub-headings">What does this platform do?</h3>
                                     <p className="mt-4">
                                         This platform helps contributors find open source projects that are a good fit for their skills and abilities, so that they can start contributing to open source. All of these projects have been vetted to ensure that they’re active, healthy, and ready for beginner contributions.
@@ -55,7 +55,7 @@ export default class About extends React.PureComponent {
                                             alt="sectionTwo.svg"
                                         />
                                     </div>
-                                    <div className="col-md-6 mt-5 mb-5">
+                                    <div className="col-md-6 mt-5 mb-5  my-auto">
                                         <h3 className="about-sub-headings">Why do you need the platform?</h3>
                                         <p className="mt-4">
                                             Our research found that while many people are interested in contributing to open source, one of the biggest barriers to getting started was difficulty in finding an appropriate project that was a match for their skills, and abilities. This platform acts as a filtering system to help people overcome that initial obstacle and find a project that is right for them.
@@ -68,7 +68,7 @@ export default class About extends React.PureComponent {
                     <section className=" container">
                         <div className="container">
                             <div className="row mt-5 mb-5">
-                                <div className="col-md-6">
+                                <div className="col-md-6  my-auto">
                                     <h3 className="about-sub-headings">How to use the platform?</h3>
                                     <p className="mt-4">
                                         On the home page describe the programming languages you're comfortable using, once you put in a language, the site will ask you more about what kinds of issues you're looking for. It will then search the projects and surface issues that match your criteria. Give it a try!
