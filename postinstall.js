1c99f870047a0bd3454ceeda2a51766a0cb8acfe
Alexandros Dorodoulis <aldor94@gmail.com>
2019-03-20 10:29:34 +0100

    chore: Cleanup postinstall msg

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,7 @@
-const package = require('./package.json')
+const package = require("./package.json");
 
 var version = process.versions.node.split(".");
 if (version[0] < 8) {
-	// tslint:disable-next-line:no-console
-	console.log("❗️❗️ This project requires Node.js version >= 8.10 ❗️❗️");
-} else {
-	// tslint:disable-next-line:no-console
-	console.log("Got a problem during installation? Don't forget to report it in our repository");
+  // tslint:disable-next-line:no-console
+  console.log("❗️❗️ This project requires Node.js version >= 8.10 ❗️❗️");
 }
